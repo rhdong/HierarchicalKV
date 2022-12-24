3d5720bcb9c63a7ecd17c239d2185c9406460b29
rhdong <rhdong2017@gmail.com>
2022-12-24 12:13:22 +0800

    d3

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -703,12 +703,12 @@ __forceinline__ __device__ unsigned find_unoccupied_in_bucket(
 
 template <class K>
 __device__ constexpr K get_empty_key() {
-  return EMPTY_KEY;
+  return static_cast<K>(EMPTY_KEY);
 }
 
 template <class K>
 __device__ constexpr K get_reclaimed_key() {
-  return RECLAIM_KEY;
+  return static_cast<K>(RECLAIM_KEY);
 }
 
 template <class K, class V, class M, size_t DIM, uint32_t TILE_SIZE = 4>
