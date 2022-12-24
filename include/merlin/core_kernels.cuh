c0153b653186eac7c87bcc914de8d9f151864ff7
rhdong <rhdong2017@gmail.com>
2022-12-24 12:19:50 +0800

    d5

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -736,12 +736,12 @@ __forceinline__ __device__ unsigned find_unoccupied_and_occupy_in_bucket(
       int src_lane = __ffs(unoccupied_vote) - 1;
       if (src_lane == g.thread_rank()) {
         if (bucket->keys[key_offset].compare_exchange_strong(
-                get_empty_key<K>(), find_key, cuda::std::memory_order_relaxed)) {
+                get_empty_key<uint64_t>(), find_key, cuda::std::memory_order_relaxed)) {
           return unoccupied_vote;
         }
 
         if (bucket->keys[key_offset].compare_exchange_strong(
-                get_reclaimed_key<K>(), find_key, cuda::std::memory_order_relaxed)) {
+                get_reclaimed_key<uint64_t>(), find_key, cuda::std::memory_order_relaxed)) {
           return unoccupied_vote;
         }
       }
