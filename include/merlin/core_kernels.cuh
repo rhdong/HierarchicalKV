b11eec03f5e5f854ebd93637a153e96cf6ea07cc
rhdong <rhdong2017@gmail.com>
2022-12-25 12:28:03 +0800

    d8

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -705,6 +705,8 @@ __forceinline__ __device__ unsigned try_occupy(
     cg::thread_block_tile<TILE_SIZE> g,
     const Bucket<K, V, M, DIM>* __restrict bucket, K find_key,
     uint32_t key_offset) {
+  
+  unsigned unoccupied_vote = 0;
   K expected_key = static_cast<K>(EMPTY_KEY);
   if (bucket->keys[key_offset].compare_exchange_strong(
           expected_key, find_key, cuda::std::memory_order_relaxed)) {
