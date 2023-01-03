ef00c79e3e79458dd810a79266476078bf052c0e
rhdong <rhdong2017@gmail.com>
2023-01-03 16:41:31 +0800

    d2

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -922,11 +922,11 @@ __global__ void upsert_kernel_with_io(
       src_lane = (bucket->min_pos % TILE_SIZE);
       key_pos = bucket->min_pos;
 
-      lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
       if (rank == src_lane) {
         bucket->keys[key_pos].store(insert_key, cuda::std::memory_order_relaxed);
         update_meta(bucket, key_pos, metas, key_idx);
       }
+      lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
       refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket, bucket_max_size);
       copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
                                      bucket->vectors + key_pos);
