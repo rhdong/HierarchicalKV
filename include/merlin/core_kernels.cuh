522514dc32a7d01fe33c5b3fb885ddd57473c7e3
rhdong <rhdong2017@gmail.com>
2023-01-04 13:44:07 +0800

    d5

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -832,13 +832,13 @@ __global__ void upsert_kernel_with_io(
                                     cuda::std::memory_order_relaxed);
         update_meta(bucket, key_pos, metas, key_idx);
       }
-      lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
       if (local_size >= bucket_max_size) {
         refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
                                                      bucket_max_size);
       }
-//      copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
-//                                     bucket->vectors + key_pos);
+      lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
+      copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
+                                     bucket->vectors + key_pos);
 
       unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
       continue;
@@ -865,13 +865,13 @@ __global__ void upsert_kernel_with_io(
                                       cuda::std::memory_order_relaxed);
           update_meta(bucket, key_pos, metas, key_idx);
         }
-        lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
         if (local_size >= bucket_max_size) {
           refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
                                                        bucket_max_size);
         }
-//        copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
-//                                       bucket->vectors + key_pos);
+        lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
+        copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
+                                       bucket->vectors + key_pos);
 
         unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
 
@@ -908,8 +908,8 @@ __global__ void upsert_kernel_with_io(
                                                          bucket_max_size);
           }
           lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
-//          copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
-//                                         bucket->vectors + key_pos);
+          copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
+                                         bucket->vectors + key_pos);
           unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
           break;
         } else if (status == InsertResult::DUPLICATE) {
