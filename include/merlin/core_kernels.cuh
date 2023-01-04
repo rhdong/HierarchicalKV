2fc3650d23514ab02cfa2fc8e5942d6206c87c50
rhdong <rhdong2017@gmail.com>
2023-01-04 13:40:19 +0800

    d3

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -18,6 +18,8 @@
 
 #include <cooperative_groups.h>
 #include <cooperative_groups/reduce.h>
+#include <cuda_pipeline.h>
+
 #include <thread>
 #include <vector>
 #include "types.cuh"
@@ -863,15 +865,15 @@ __global__ void upsert_kernel_with_io(
                                       cuda::std::memory_order_relaxed);
           update_meta(bucket, key_pos, metas, key_idx);
         }
-//        lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
+        lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
         if (local_size >= bucket_max_size) {
           refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
                                                        bucket_max_size);
         }
-        copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
-                                       bucket->vectors + key_pos);
+//        copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
+//                                       bucket->vectors + key_pos);
 
-//        unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
+        unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
 
         break;
       }
@@ -900,14 +902,15 @@ __global__ void upsert_kernel_with_io(
             atomicAdd(&(buckets_size[bkt_idx]), 1);
           }
           local_size++;
+
+          lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
           if (local_size >= bucket_max_size) {
             refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
                                                          bucket_max_size);
           }
-//          lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
-          copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
-                                         bucket->vectors + key_pos);
-//          unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
+//          copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
+//                                         bucket->vectors + key_pos);
+          unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
           break;
         } else if (status == InsertResult::DUPLICATE) {
           break;
@@ -927,11 +930,11 @@ __global__ void upsert_kernel_with_io(
                                     cuda::std::memory_order_relaxed);
         update_meta(bucket, key_pos, metas, key_idx);
       }
-//      lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
+      lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
       refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket, bucket_max_size);
-      copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
-                                     bucket->vectors + key_pos);
-//      unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
+//      copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
+//                                     bucket->vectors + key_pos);
+      unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx]);
     }
   }
 }
