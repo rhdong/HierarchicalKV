dc7bc8ee33b9447b51c502350fbed8d08921742b
rhdong <rhdong2017@gmail.com>
2023-01-12 13:51:33 +0800

    d1

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -661,6 +661,14 @@ __forceinline__ __device__ void find_in_bucket_with_io(
       lock<Mutex, TILE_SIZE, true>(g, *klock, src_lane);
       copy_vector<V, DIM, TILE_SIZE>(g, value, dst);
       unlock<Mutex, TILE_SIZE, true>(g, *klock, src_lane);
+      
+      if (rank == src_lane) {
+        update_meta(bucket, key_pos, metas, key_idx);
+      }
+      if (local_size >= bucket_max_size) {
+        refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
+                                                     bucket_max_size);
+      }
       return;
     }
 
@@ -1104,19 +1112,19 @@ __global__ void scatter_update_with_io(
         g, bucket, values + key_idx, &(table->locks[bkt_idx]), insert_key,
         tile_offset, start_idx, bucket_max_size);
 
-    if (found_vote) {
-      src_lane = __ffs(found_vote) - 1;
-      key_pos = (start_idx + tile_offset + src_lane) & (bucket_max_size - 1);
-      if (rank == src_lane) {
-        update_meta(bucket, key_pos, metas, key_idx);
-      }
-      if (local_size >= bucket_max_size) {
-        refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
-                                                     bucket_max_size);
-      }
-
-      continue;
-    }
+//    if (found_vote) {
+//      src_lane = __ffs(found_vote) - 1;
+//      key_pos = (start_idx + tile_offset + src_lane) & (bucket_max_size - 1);
+//      if (rank == src_lane) {
+//        update_meta(bucket, key_pos, metas, key_idx);
+//      }
+//      if (local_size >= bucket_max_size) {
+//        refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
+//                                                     bucket_max_size);
+//      }
+//
+//      continue;
+//    }
   }
 }
 
