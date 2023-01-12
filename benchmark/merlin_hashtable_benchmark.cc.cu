23f19358b98288b95e342301ae97605751098916
rhdong <rhdong2017@gmail.com>
2023-01-12 14:40:14 +0800

    d7

--- benchmark/merlin_hashtable_benchmark.cc.cu
+++ benchmark/merlin_hashtable_benchmark.cc.cu
@@ -159,7 +159,7 @@ void test_main(const size_t init_capacity = 64 * 1024 * 1024UL,
   std::chrono::duration<double> diff_erase;
 
   while (cur_load_factor < load_factor) {
-    create_continuous_keys<K, M>(h_keys, h_metas, key_num_per_op, start);
+    create_random_keys<K, M>(h_keys, h_metas, key_num_per_op);
     CUDA_CHECK(cudaMemcpy(d_keys, h_keys, key_num_per_op * sizeof(K),
                           cudaMemcpyHostToDevice));
     CUDA_CHECK(cudaMemcpy(d_metas, h_metas, key_num_per_op * sizeof(M),
