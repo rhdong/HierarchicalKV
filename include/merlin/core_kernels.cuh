592d359fd39cf244931d4db2e2c2dc4934c7e768
rhdong <rhdong2017@gmail.com>
2023-01-12 16:20:19 +0800

    d2

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -1092,6 +1092,7 @@ __global__ void scatter_update_with_io(
     size_t key_idx = t / TILE_SIZE;
 
     const K insert_key = keys[key_idx];
+    const V insert_value = values[key_idx];
 
     size_t bkt_idx = 0;
     size_t start_idx = 0;
@@ -1101,7 +1102,7 @@ __global__ void scatter_update_with_io(
                                  buckets_num, bucket_max_size);
 
     find_in_bucket_with_io<K, V, M, DIM, TILE_SIZE>(
-        g, bucket, values + key_idx, nullptr, insert_key,
+        g, bucket, &insert_value, nullptr, insert_key,
         tile_offset, start_idx, bucket_max_size);
 
     //    if (found_vote) {
