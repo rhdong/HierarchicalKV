47cc734a58200ff81d9d3cc398c4c3ee7fb3b5df
rhdong <rhdong2017@gmail.com>
2023-01-13 13:44:00 +0800

    d3

--- benchmark/merlin_hashtable_benchmark.cc.cu
+++ benchmark/merlin_hashtable_benchmark.cc.cu
@@ -282,7 +282,7 @@ int main() {
        << "### On pure HBM mode: " << endl;
   print_title();
   try {
-    test_main<1>(8 * 1024 * 1024UL, key_num_per_op, 16, 0.50);
+    test_main<4>(64 * 1024 * 1024UL, key_num_per_op, 16, 0.50);
 //    test_main<4>(64 * 1024 * 1024UL, key_num_per_op, 32, 0.75);
 //    test_main<4>(64 * 1024 * 1024UL, key_num_per_op, 32, 1.00);
 //
