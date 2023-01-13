bfc1073f0f292e483bc697b16a289012b25df738
rhdong <rhdong2017@gmail.com>
2023-01-13 14:21:46 +0800

    d9

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -642,7 +642,7 @@ __forceinline__ __device__ unsigned find_in_bucket(
 template <class K, class V, class M, size_t DIM, uint32_t TILE_SIZE = 4>
 __forceinline__ __device__ void find_in_bucket_with_io(
     cg::thread_block_tile<TILE_SIZE> g,
-    const K* __restrict bucket_keys, const V* __restrict bucket_vectors, const V* value, Mutex* klock,
+    const AtomicKey<K>* __restrict bucket_keys, const V* __restrict bucket_vectors, const V* value, Mutex* klock,
     const K find_key, uint32_t& tile_offset, const uint32_t start_idx,
     const size_t bucket_max_size) {
   uint32_t key_pos = 0;
