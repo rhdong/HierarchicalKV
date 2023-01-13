b35cd6d7f46ad4e8ea4b27d554a29918f3382b72
rhdong <rhdong2017@gmail.com>
2023-01-13 16:33:26 +0800

    d8

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -678,14 +678,12 @@ __device__ void find_in_bucket_with_io(
     const K& find_key, uint32_t& tile_offset, const uint32_t& start_idx,
     const size_t& bucket_max_size) {
   uint32_t key_pos = 0;
-  uint32_t quar_step = 0;
 
 //#pragma unroll
   for (tile_offset = 0; tile_offset < bucket_max_size;
        tile_offset += TILE_SIZE) {
     key_pos =
-        (start_idx + tile_offset + quar_step + g.thread_rank()) & (bucket_max_size - 1);
-    quar_step++;
+        (start_idx + tile_offset + g.thread_rank()) & (bucket_max_size - 1);
     auto const current_key =
         bucket_keys[key_pos].load(cuda::std::memory_order_relaxed);
     auto const found_vote = g.ballot(find_key == current_key);
