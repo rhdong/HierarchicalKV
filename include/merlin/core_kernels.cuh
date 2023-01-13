0c1a7c738f4023306ea1a5b3d6f9e99da43671e6
rhdong <rhdong2017@gmail.com>
2023-01-13 14:27:45 +0800

    d1

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -642,7 +642,7 @@ __forceinline__ __device__ unsigned find_in_bucket(
 template <class K, class V, class M, size_t DIM, uint32_t TILE_SIZE = 4>
 __forceinline__ __device__ void find_in_bucket_with_io(
     cg::thread_block_tile<TILE_SIZE> g,
-    const AtomicKey<K>* __restrict bucket_keys, const V* __restrict bucket_vectors, const V* value, Mutex* klock,
+    const AtomicKey<K>* __restrict bucket_keys, V* __restrict bucket_vectors, const V* value, Mutex* klock,
     const K find_key, uint32_t& tile_offset, const uint32_t start_idx,
     const size_t bucket_max_size) {
   uint32_t key_pos = 0;
@@ -657,7 +657,7 @@ __forceinline__ __device__ void find_in_bucket_with_io(
     auto const found_vote = g.ballot(find_key == current_key);
     if (found_vote) {
       auto const src_lane = __ffs(found_vote) - 1;
-      const V* dst = bucket_vectors + g.shfl(key_pos, src_lane);
+      auto dst = bucket_vectors + g.shfl(key_pos, src_lane);
       //      lock<Mutex, TILE_SIZE, true>(g, *klock, src_lane);
       copy_vector<V, DIM, TILE_SIZE>(g, value, dst);
       //      unlock<Mutex, TILE_SIZE, true>(g, *klock, src_lane);
