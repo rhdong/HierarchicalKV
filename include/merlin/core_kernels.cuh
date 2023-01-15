02fc3ce1875bbe01531baa2fe76f0fbe80c670c3
rhdong <rhdong2017@gmail.com>
2023-01-15 17:08:19 +0800

    d4

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -1142,11 +1142,11 @@ __global__ void scatter_update_with_io(
         bucket_max_size);
 
     if (found_vote) {
-      auto const src_lane = __ffs(found_vote) - 1;
-      uint32_t key_pos = g.shfl(key_pos, src_lane);
-      key_pos =
-          (start_idx + tile_offset + g.thread_rank()) & (bucket_max_size - 1);
-      auto dst = bucket->vectors + key_pos;
+//      auto const src_lane = __ffs(found_vote) - 1;
+//      uint32_t key_pos = g.shfl(key_pos, src_lane);
+//      key_pos =
+//          (start_idx + tile_offset + g.thread_rank()) & (bucket_max_size - 1);
+//      auto dst = bucket->vectors + key_pos;
 //      lock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx], src_lane);
 //      copy_vector<V, DIM, TILE_SIZE>(g, insert_value, dst);
 //      unlock<Mutex, TILE_SIZE, true>(g, table->locks[bkt_idx], src_lane);
