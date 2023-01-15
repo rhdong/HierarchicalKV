8f416fc5a6f4fbd59025a1c5ff229b976c903ade
rhdong <rhdong2017@gmail.com>
2023-01-15 16:16:43 +0800

    d4

--- include/merlin_hashtable.cuh
+++ include/merlin_hashtable.cuh
@@ -251,19 +251,19 @@ class HashTable {
       return;
     }
 
-    while (!reach_max_capacity_ &&
-           fast_load_factor(n) > options_.max_load_factor) {
-      reserve(capacity() * 2);
-    }
-
-    if (!ignore_evict_strategy) {
-      check_evict_strategy(metas);
-    }
-
-    std::shared_lock<std::shared_timed_mutex> lock(mutex_, std::defer_lock);
-    if (!reach_max_capacity_) {
-      lock.lock();
-    }
+//    while (!reach_max_capacity_ &&
+//           fast_load_factor(n) > options_.max_load_factor) {
+//      reserve(capacity() * 2);
+//    }
+//
+//    if (!ignore_evict_strategy) {
+//      check_evict_strategy(metas);
+//    }
+//
+//    std::shared_lock<std::shared_timed_mutex> lock(mutex_, std::defer_lock);
+//    if (!reach_max_capacity_) {
+//      lock.lock();
+//    }
 
     if (is_fast_mode()) {
       const size_t block_size = options_.block_size;
