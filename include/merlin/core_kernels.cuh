e19b7063c11fac87d2ae599f30cb16a4a3a5f6d9
rhdong <rhdong2017@gmail.com>
2023-01-15 15:36:03 +0800

    d5

--- include/merlin/core_kernels.cuh
+++ include/merlin/core_kernels.cuh
@@ -850,7 +850,7 @@ __global__ void upsert_kernel_with_io(
 
     found_vote = find_in_bucket_with_io<K, V, M, DIM, TILE_SIZE>(
         g, bucket->keys, bucket->vectors, insert_value,
-        &(table->locks[bkt_idx]), insert_key, tile_offset, start_idx,
+        insert_key, tile_offset, start_idx,
         bucket_max_size);
 
     if (found_vote) {
