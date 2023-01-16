668839dd47dc0df0293b03b5b05b6714271a4f28
rhdong <rhdong2017@gmail.com>
2023-01-16 12:05:51 +0800

    d8

--- benchmark/merlin_hashtable_benchmark.cc.cu
+++ benchmark/merlin_hashtable_benchmark.cc.cu
@@ -170,7 +170,7 @@ void test_main(const size_t init_capacity = 64 * 1024 * 1024UL,
                             reinterpret_cast<float*>(d_vectors), d_metas,
                             stream);
     CUDA_CHECK(cudaStreamSynchronize(stream));
-    for(int i = 0;i < 10; i++){
+    for(int i = 0;i < 1; i++){
       start_insert_or_assign = std::chrono::steady_clock::now();
       table->insert_or_assign(key_num_per_op, d_keys,
                               reinterpret_cast<float*>(d_vectors), d_metas,
