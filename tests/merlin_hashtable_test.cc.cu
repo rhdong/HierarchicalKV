bea3661b75d8f3aa64e88223de9bb6bc5131e53a
rhdong <rhdong2017@gmail.com>
2023-03-04 12:00:25 +0800

    d3

--- tests/merlin_hashtable_test.cc.cu
+++ tests/merlin_hashtable_test.cc.cu
@@ -1959,7 +1959,7 @@ void test_evict_strategy_customized_correct_rate(size_t max_hbm_for_vectors,
 //                    static_cast<V>(h_keys_temp[i] * 0.00001))
 //              << "i = " << i << ", dim = " << j << ", key = " << h_keys_temp[i]
 //              << ", j + 1 = " << h_vectors_temp[i * options.dim + j + 1];
-        if((h_vectors_temp[i * options.dim + 0] != static_cast<V>(h_keys_temp[i] * 0.00001))) { // || 195000 == h_keys_temp[i]|| 945731 == h_keys_temp[i]) {
+        if((h_vectors_temp[i * options.dim + 0] != static_cast<V>(h_keys_temp[i] * 0.00001)) || 195000 == h_keys_temp[i]|| 945731 == h_keys_temp[i]) {
           std::cout << "i = " << i << ", dim = " << 0 << ", key = " << h_keys_temp[i] << ", j + 1 = " << std::endl;
           for(int k = 0; k < DIM ; k++){
              std::cout << "\t" << h_vectors_temp[i * options.dim + 0 + k];
