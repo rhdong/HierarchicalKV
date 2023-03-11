5ae22db8dc39136310fdc6489279fc0c4856a0e5
rhdong <rhdong2017@gmail.com>
2023-03-11 15:49:24 +0800

    dt

--- benchmark/merlin_hashtable_benchmark.cc.cu
+++ benchmark/merlin_hashtable_benchmark.cc.cu
@@ -260,7 +260,7 @@ void print_title() {
 }
 
 int main() {
-  size_t key_num_per_op = 1 * 1024 * 1024UL;
+  size_t key_num_per_op = 16 * 1024UL;
   cudaDeviceProp props;
   CUDA_CHECK(cudaGetDeviceProperties(&props, 0));
   cout << endl
@@ -276,9 +276,9 @@ int main() {
        << "### On pure HBM mode: " << endl;
   print_title();
   try {
-    test_main(4, 64 * 1024 * 1024UL, key_num_per_op, 32, 0.50);
-    test_main(4, 64 * 1024 * 1024UL, key_num_per_op, 32, 0.75);
-    test_main(4, 64 * 1024 * 1024UL, key_num_per_op, 32, 1.00);
+    test_main(4, 1 * 1024 * 1024UL, key_num_per_op, 32, 0.50);
+    test_main(4, 1 * 1024 * 1024UL, key_num_per_op, 32, 0.75);
+    test_main(4, 1 * 1024 * 1024UL, key_num_per_op, 32, 1.00);
 
     test_main(16, 64 * 1024 * 1024UL, key_num_per_op, 16, 0.50);
     //    test_main(16, 64 * 1024 * 1024UL, key_num_per_op, 16, 0.75);
