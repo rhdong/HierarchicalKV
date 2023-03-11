010b947fb9f402c4a778fe24b6072f85ea32163a
rhdong <rhdong2017@gmail.com>
2023-03-11 16:14:10 +0800

    d5

--- benchmark/merlin_hashtable_benchmark.cc.cu
+++ benchmark/merlin_hashtable_benchmark.cc.cu
@@ -280,7 +280,7 @@ int main() {
 //    test_main(4, 64 * 1024 * 1024UL, key_num_per_op, 32, 0.75);
 //    test_main(4, 64 * 1024 * 1024UL, key_num_per_op, 32, 1.00);
 
-    test_main(16, 64 * 1024 * 1024UL, key_num_per_op, 16, 0.50);
+    test_main(16, 64 * 1024 * 1024UL, key_num_per_op, 32, 0.50);
     //    test_main(16, 64 * 1024 * 1024UL, key_num_per_op, 16, 0.75);
     //    test_main(16, 64 * 1024 * 1024UL, key_num_per_op, 16, 1.00);
     //
