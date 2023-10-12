855c3003fc3d1a4d6132dbf60b830f551b6b6030
rhdong <rhdong2017@gmail.com>
2023-10-12 16:21:28 +0800

    d6

--- tests/merlin_hashtable_test.cc.cu
+++ tests/merlin_hashtable_test.cc.cu
@@ -3271,10 +3271,10 @@ void test_bucket_size(bool load_scores = true) {
 }
 
 void test_duplicated_keys(size_t max_hbm_for_vectors) {
-  constexpr uint64_t INIT_CAPACITY = 64 * 1024 * 1024UL;
+  constexpr uint64_t INIT_CAPACITY = 32 * 1024 * 1024UL;
   constexpr uint64_t MAX_CAPACITY = INIT_CAPACITY;
-  constexpr uint64_t KEY_NUM = 1024UL;
-  constexpr uint64_t TEST_TIMES = 3;
+  constexpr uint64_t KEY_NUM = 1024 * 512UL;
+  constexpr uint64_t TEST_TIMES = 32;
 
   K* h_keys;
   S* h_scores;
